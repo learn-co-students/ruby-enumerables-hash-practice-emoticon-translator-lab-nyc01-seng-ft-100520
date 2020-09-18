998950c418eeb2286e74639e6189307cb5aa227b
Kevin Huang <khuang159@gmail.com>
2020-09-18 19:57:57 +0000

    Done.

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,13 +1,36 @@
 # require modules here
+require "yaml"
+require 'pry'
 
-def load_library
-  # code goes here
+def load_library(yml)
+  flex = YAML.load_file(yml) 
+
+  flex.each_with_object({}) do |(key, value), i|
+  i[key] = {:english => value[0],:japanese => value[1]}
+  i
+  end
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(yml,emo)
+  flex = load_library(yml)
+  
+  flex.each do |key,value|
+    if value[:english] == emo 
+      return value[:japanese]
+  end
+end
+return "Sorry, that emoticon was not found"
 end
 
-def get_english_meaning
-  # code goes here
+# get_japanese_emoticon("./lib/emoticons.yml", "ヽ(ｏ`皿′ｏ)ﾉ")
+
+ def get_english_meaning(yml,emo)
+  flex = load_library(yml)
+  flex.each do |key,value|
+    if value[:japanese] == emo
+     return  key
+    end
+ end
+  return "Sorry, that emoticon was not found"
 end
+ get_english_meaning("./lib/emoticons.yml", "(＾ｖ＾)")
